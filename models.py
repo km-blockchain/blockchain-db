24166f404cfb7a02001123d683cba36ae6be02ff
user < >
2019-08-10 06:19:54 +0300

    Добавить баланс

--- models.py
+++ models.py
@@ -6,3 +6,4 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
+    balance = db.Column(db.Float, default=0)
