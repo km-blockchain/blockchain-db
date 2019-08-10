5592653464c4a266208b1dc17ca29902f8e35045
Ученик <>
2019-08-10 12:11:15 +0700

    Добавить получение транзакций

--- routes.py
+++ routes.py
@@ -9,3 +9,9 @@ def user():
     if not user:
         return abort(404)
     return jsonify(id=user.id, name=user.name)
+@app.route('/trans')
+def trans():
+    trans = Transaction.querty.filter_by(id=request.args['trans_id']).first()
+    if not user:
+        return abort(404)
+    return jsonify(trans_object(trans))
