ad311e01dd0975f5cbd2cac37717e6f9dd522c0d
Ученик <>
2019-08-10 12:15:14 +0700

    фикс мержа

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from app import app
 from models import *
-from flask import jsonify, request, abort
+from flask import jsonify, request, abort, redirect, url_for
 
 
 @app.route('/user')
@@ -9,13 +9,14 @@ def user():
     if not user:
         return abort(404)
     return jsonify(id=user.id, name=user.name)
+
+
 @app.route('/register')
 def register():
     user = User(name=request.args['name'], password=request.args['password'])
     db.session.add(user)
     db.session.commit()
     return redirect(url_for('user'))
-
 
 
 @app.route('/trans')
